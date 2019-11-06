e9ebb4bf2b1469a9e61ac94e73d837db6a8045b9
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-06 15:20:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  numbers = [1,2,3]
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
+
+# def square_array(array)
+# new_array=[]
+# counter=0
+# while counter<array.length do
+#   new_array<<array[counter]**2
+#   counter+=1
+# end
+# return new_array
+# end
